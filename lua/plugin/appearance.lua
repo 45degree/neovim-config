903d957455a6895d70ec1eb010102190fb067f0f
45degree <1098902652@qq.com>
2022-04-06 21:15:35 +0800

    align the content in dashboard

--- lua/plugin/appearance.lua
+++ lua/plugin/appearance.lua
@@ -32,11 +32,11 @@ return function(use)
                 [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
             }
             dashboard.section.buttons.val = {
-                dashboard.button( "f", "   file", ":Telescope find_files <cr>"),
+                dashboard.button( "f", "  file", ":Telescope find_files <cr>"),
                 dashboard.button( "h", "ﭯ  History", ":Telescope oldfiles <cr>"),
                 dashboard.button( "c", "  Color" , ":Telescope colorscheme <cr>"),
-                dashboard.button( "b", "   marks" , ":Telescope marks <cr>"),
-                dashboard.button( "b", "   word" , ":Telescope live_grep <cr>"),
+                dashboard.button( "b", "  marks" , ":Telescope marks <cr>"),
+                dashboard.button( "b", "  word" , ":Telescope live_grep <cr>"),
             }
             alpha.setup(dashboard.config)
         end
