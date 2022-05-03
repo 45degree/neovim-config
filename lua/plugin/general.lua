1634b9874a7dc6f4684f6808a3156f48a21608fb
45degree <1098902652@qq.com>
2022-05-03 14:08:15 +0800

    enable telescope integrate project.nvim

--- lua/plugin/general.lua
+++ lua/plugin/general.lua
@@ -344,6 +344,7 @@ return function(use)
         "nvim-telescope/telescope.nvim",
         config = function()
             require("telescope").setup()
+            require('telescope').load_extension('projects')
         end
     }
 
