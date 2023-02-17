f6540c4ba2626a1a16785bf4934509dfa25edc25
45degree <1098902652@qq.com>
2023-02-17 12:22:40 +0800

    update config
    1. remove startup time plugin
    2. add lastspace.nvim plugin

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -25,10 +25,15 @@ return {
     end
   },
 
-  -- 启动速度
   {
-    "dstein64/vim-startuptime",
-    cmd = "StartupTime"
+    'ethanholz/nvim-lastplace',
+    config = function()
+      require'nvim-lastplace'.setup {
+        lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+        lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
+        lastplace_open_folds = true
+      }
+    end
   },
 
   {
