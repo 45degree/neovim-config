67f0a64eb76864919913a7e74c062ed4d0b9703b
45degree <1098902652@qq.com>
2023-05-06 19:40:12 +0800

    fix the chinese font on goneovim

--- init.lua
+++ init.lua
@@ -35,7 +35,7 @@ require('config.dap.dap-config').setup()
 
 if vim.fn.exists('g:gonvim_running') == 1 then
   vim.opt.guifont = 'Maple Mono:h10'
-  vim.opt.guifontwide = 'Symbols Nerd Font,Sarasa Fixed SC:h10'
+  vim.opt.guifontwide = 'Symbols Nerd Font:h10,Sarasa Fixed SC:h10'
 elseif vim.fn.exists('g:neovide') == 1 then
   if vim.fn.has('win32') == 1 then
     vim.opt.guifont = 'Maple Mono,Sarasa Fixed SC,Symbols Nerd Font 2048-em:h10'
