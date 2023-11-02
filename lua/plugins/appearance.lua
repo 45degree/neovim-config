17472bcd1ae100043833849cbed8f0994a664f78
45degree <1098902652@qq.com>
2023-11-02 18:30:29 +0800

    update config

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -61,13 +61,13 @@ return {
     config = function()
       local theme = OptionConfig.getColorTheme()
       if
-          theme == 'nightfox'
-          or theme == 'dayfox'
-          or theme == 'dawnfox'
-          or theme == 'duskfox'
-          or theme == 'nordfox'
-          or theme == 'terafox'
-          or theme == 'carbonfox'
+        theme == 'nightfox'
+        or theme == 'dayfox'
+        or theme == 'dawnfox'
+        or theme == 'duskfox'
+        or theme == 'nordfox'
+        or theme == 'terafox'
+        or theme == 'carbonfox'
       then
         require('config.colorTheme.nightfox')
         vim.cmd('colorscheme ' .. theme)
@@ -226,11 +226,15 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       vim.o.foldcolumn = '1' -- '0' is not bad
-      vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
       vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-      require('ufo').setup({})
+      require('ufo').setup({
+        provider_selector = function(bufnr, filetype, buftype)
+          return { 'treesitter', 'indent' }
+        end,
+      })
 
       -- disable fold in this file
       vim.api.nvim_create_autocmd('FileType', {
@@ -257,9 +261,9 @@ return {
         ft_ignore = { 'neo-tree', 'toggleterm', 'Outline', 'alpha' },
         bt_ignore = { 'nofile', 'prompt' },
         segments = {
-          { text = { builtin.foldfunc },                                       click = 'v:lua.ScFa' },
-          { sign = { name = { '.*' }, maxwidth = 1 },                          click = 'v:lua.ScSa' },
-          { text = { builtin.lnumfunc },                                       click = 'v:lua.ScLa' },
+          { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
+          { sign = { name = { '.*' }, maxwidth = 1 }, click = 'v:lua.ScSa' },
+          { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
           { sign = { namespace = { 'gitsign*' }, maxwidth = 2, colwidth = 1 }, click = 'v:lua.ScSa' },
         },
       })
--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -217,6 +217,10 @@ return {
       require('overseer').setup({
         strategy = 'toggleterm',
         task_list = { direction = 'bottom' },
+        default_vscode = {
+          'default',
+          { 'on_result_diagnostics_quickfix', set_empty_results = true },
+        },
       })
     end,
   },
