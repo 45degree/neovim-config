97a41b65dc33bdb24b602e9142db42a7a93c1e15
45degree <1098902652@qq.com>
2024-05-19 17:09:35 +0800

    remove neogit
    use lazygit instead neogit

--- lua/plugins/git/diffview-nvim.lua
+++ lua/plugins/git/diffview-nvim.lua
@@ -1,24 +1,3 @@
-local opts = {
-  view = {
-    default = {
-      -- Config for changed files, and staged files in diff views.
-      layout = 'diff2_horizontal',
-      winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
-    },
-    merge_tool = {
-      -- Config for conflicted files in diff views during a merge or rebase.
-      layout = 'diff3_mixed',
-      disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
-      winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
-    },
-    file_history = {
-      -- Config for changed files in file history views.
-      layout = 'diff2_horizontal',
-      winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
-    },
-  },
-}
-
 return {
   'sindrets/diffview.nvim',
   cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
@@ -26,6 +5,40 @@ return {
     'nvim-lua/plenary.nvim',
   },
   config = function()
-    require('diffview').setup(opts)
+    local actions = require('diffview.actions')
+    require('diffview').setup({
+      view = {
+        default = {
+          -- Config for changed files, and staged files in diff views.
+          layout = 'diff2_horizontal',
+          winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
+        },
+        merge_tool = {
+          -- Config for conflicted files in diff views during a merge or rebase.
+          layout = 'diff3_mixed',
+          disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers while in the view.
+          winbar_info = true, -- See ':h diffview-config-view.x.winbar_info'
+        },
+        file_history = {
+          -- Config for changed files in file history views.
+          layout = 'diff2_horizontal',
+          winbar_info = false, -- See ':h diffview-config-view.x.winbar_info'
+        },
+      },
+      keymaps = {
+        view = {
+          { 'n', 'q', actions.close, { desc = 'Close diffview' } },
+          { 'n', '<Esc>', actions.close, { desc = 'Close diffview' } },
+        },
+        file_panel = {
+          { 'n', 'q', actions.close, { desc = 'Close diffview' } },
+          { 'n', '<Esc>', actions.close, { desc = 'Close diffview' } },
+        },
+        file_history_panel = {
+          { 'n', 'q', actions.close, { desc = 'Close diffview' } },
+          { 'n', '<Esc>', actions.close, { desc = 'Close diffview' } },
+        },
+      },
+    })
   end,
 }
