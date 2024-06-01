3e118e07f3dad0c0ce8c652531a3493ba81fee0f
45degree <1098902652@qq.com>
2024-06-01 20:33:01 +0800

    update nvim-dap-ui config

--- lua/plugins/dap/nvim-dap-ui.lua
+++ lua/plugins/dap/nvim-dap-ui.lua
@@ -11,25 +11,12 @@ return {
     local config = require('config')
 
     dapui.setup({
-      icons = { expanded = '', collapsed = '' },
-      mappings = {
-        -- Use a table to apply multiple mappings
-        expand = { '<2-LeftMouse>', '<CR>' },
-        open = 'o',
-        remove = 'd',
-        edit = 'e',
-        repl = 'r',
-        toggle = 't',
-      },
+      icons = { expanded = '', collapsed = '', current_frame = '' },
       layouts = {
         {
           -- You can change the order of elements in the sidebar
           elements = {
-            -- Provide as ID strings or tables with "id" and "size" keys
-            {
-              id = 'scopes',
-              size = 0.35, -- Can be float or integer > 1
-            },
+            { id = 'scopes', size = 0.35 },
             { id = 'stacks', size = 0.35 },
             { id = 'watches', size = 0.15 },
             { id = 'breakpoints', size = 0.15 },
@@ -38,22 +25,12 @@ return {
           position = 'left', -- Can be "left", "right", "top", "bottom"
         },
         {
-          elements = {
-            'repl',
-            'console',
-          },
+          elements = { 'repl', 'console' },
           size = 0.25,
           position = 'bottom', -- Can be "left", "right", "top", "bottom"
         },
       },
-      floating = {
-        max_height = nil, -- These can be integers or a float between 0 and 1.
-        max_width = nil, -- Floats will be treated as percentage of your screen.
-        border = config.border, -- Border style. Can be "single", "double" or "rounded"
-        mappings = {
-          close = { 'q', '<Esc>' },
-        },
-      },
+      floating = { border = config.border },
       windows = { indent = 1 },
     })
 
