1d8d8c629e325a3a0e8fb6cb02ec42e51903da32
45degree <1098902652@qq.com>
2024-06-02 22:57:27 +0800

    fix error when load lazydev.nvim

--- lua/plugins/lsp/lazydev-nvim.lua
+++ lua/plugins/lsp/lazydev-nvim.lua
@@ -3,16 +3,19 @@ return {
     'folke/lazydev.nvim',
     ft = 'lua', -- only load on lua files
     enabled = false,
-    opts = {
-      library = {
-        vim.env.LAZY .. '/luvit-meta/library', -- see below
-        vim.env.LAZY .. '/mason.nvim/lua',
-        vim.env.LAZY .. '/plenary.nvim/lua',
-        -- You can also add plugins you always want to have loaded.
-        -- Useful if the plugin has globals or types you want to use
-        -- vim.env.LAZY .. "/LazyVim", -- see below
-      },
-    },
+    config = function()
+      local opts = {
+        library = {
+          vim.env.LAZY .. '/luvit-meta/library', -- see below
+          vim.env.LAZY .. '/mason.nvim/lua',
+          vim.env.LAZY .. '/plenary.nvim/lua',
+          -- You can also add plugins you always want to have loaded.
+          -- Useful if the plugin has globals or types you want to use
+          -- vim.env.LAZY .. "/LazyVim", -- see below
+        },
+      }
+      require('lazydev.nvim').setup(opts)
+    end,
   },
   { 'Bilal2453/luvit-meta', lazy = true, enabled = false },
 }
