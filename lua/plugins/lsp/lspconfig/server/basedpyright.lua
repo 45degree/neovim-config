14b9e97f868770880ec404ba9df767b15d5418ea
45degree <1098902652@qq.com>
2024-11-19 22:19:48 +0800

    update basedpyright config

---
+++ lua/plugins/lsp/lspconfig/server/basedpyright.lua
@@ -0,0 +1,19 @@
+return function(opts)
+  opts.settings = {
+    basedpyright = {
+      analysis = {
+        typeCheckingMode = 'basic',
+        autoImportCompletions = true,
+        diagnosticSeverityOverrides = {
+          reportUnusedImport = 'information',
+          reportUnusedFunction = 'information',
+          reportUnusedVariable = 'information',
+          reportGeneralTypeIssues = 'none',
+          reportOptionalMemberAccess = 'none',
+          reportOptionalSubscript = 'none',
+          reportPrivateImportUsage = 'none',
+        },
+      },
+    },
+  }
+end
