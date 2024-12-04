66149dfc1869be87b9b58f11f4360fbb3e5dfaae
45degree <1098902652@qq.com>
2024-12-04 23:04:57 +0800

    fix dashboard

--- lua/plugins/ui/snacks-nvim.lua
+++ lua/plugins/ui/snacks-nvim.lua
@@ -51,8 +51,8 @@ return {
         },
         sections = {
           { section = 'header' },
-          { section = 'keys', gap = 1 },
-          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = { 2, 2 } },
+          { section = 'keys', gap =1, padding = 2 },
+          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 2 },
           { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 2 },
           { section = 'startup' },
         },
