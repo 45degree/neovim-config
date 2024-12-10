08e939a722bd4aea626396382b08694a82fb484b
45degree <1098902652@qq.com>
2024-12-10 21:44:31 +0800

    add neotest and neotest-gtest

---
+++ lua/plugins/code/neotest-nvim.lua
@@ -0,0 +1,10 @@
+return {
+  'nvim-neotest/neotest',
+  cmd = 'Neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  config = true,
+}
---
+++ lua/plugins/lang/neotest-gtest-nvim.lua
@@ -0,0 +1,45 @@
+local c_cxx_file_extensions = {
+  ['cpp'] = true,
+  ['cppm'] = true,
+  ['cc'] = true,
+  ['cxx'] = true,
+  ['c++'] = true,
+}
+
+return {
+  {
+    'alfaix/neotest-gtest',
+    lazy = true,
+    opts = {
+      ---@param file_path string
+      is_test_file = function(file_path)
+        local sep = '/'
+        if vim.fn.has('win32') == 1 then
+          sep = '\\'
+        end
+
+        local elems = vim.split(file_path, sep, { plain = true })
+        local filename = elems[#elems]:lower()
+        if filename == '' then -- directory
+          return false
+        end
+        local extsplit = vim.split(filename, '.', { plain = true })
+        local extension = extsplit[#extsplit]
+        if not c_cxx_file_extensions[extension] then return false end
+        local stem = extsplit[#extsplit - 1]
+        vim.print(filename)
+        return vim.startswith(filename, 'test') or vim.endswith(stem, 'test')
+      end,
+    },
+  },
+
+  {
+    'nvim-neotest/neotest',
+    dependencies = 'alfaix/neotest-gtest',
+    opts = function(_, opts)
+      opts = opts or {}
+      opts.adapters = opts.adapters or {}
+      opts.adapters = vim.tbl_deep_extend('force', opts.adapters, { require('neotest-gtest') })
+    end,
+  },
+}
