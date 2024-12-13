63fcc8d59aeb2093a66a217f1a23a468dfd5fb21
45degree <1098902652@qq.com>
2024-12-14 03:25:28 +0800

    update which-key config

--- lua/plugins/editor/which-key.lua
+++ lua/plugins/editor/which-key.lua
@@ -107,10 +107,6 @@ local register = {
 return {
   'folke/which-key.nvim',
   event = 'VeryLazy',
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-  end,
   opts = { preset = 'modern' },
   config = function(_, opts)
     require('which-key').setup(opts)
