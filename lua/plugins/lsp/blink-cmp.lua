d42c9ba0796864ab3c17d383503131b8168cc9b2
45degree <1098902652@qq.com>
2025-01-08 14:03:05 +0800

    fix blink.cmp luasnip config

--- lua/plugins/lsp/blink-cmp.lua
+++ lua/plugins/lsp/blink-cmp.lua
@@ -11,16 +11,9 @@ return {
       ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
       ['<S-Tab>'] = { 'select_prev', 'snippet_backward', 'fallback' },
     },
-    snippets = {
-      expand = function(snippet) require('luasnip').lsp_expand(snippet) end,
-      active = function(filter)
-        if filter and filter.direction then return require('luasnip').jumpable(filter.direction) end
-        return require('luasnip').in_snippet()
-      end,
-      jump = function(direction) require('luasnip').jump(direction) end,
-    },
+    snippets = { preset = 'luasnip' },
     sources = {
-      default = { 'lsp', 'path', 'luasnip', 'buffer', 'lazydev' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev' },
       providers = { lazydev = { name = 'Development', module = 'lazydev.integrations.blink' } },
     },
     completion = {
