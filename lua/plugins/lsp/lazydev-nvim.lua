02af2968f67c3cf96cff7f0bf238b88dad76abc7
45degree <1098902652@qq.com>
2025-02-21 00:01:23 +0800

    add wezterm module

--- lua/plugins/lsp/lazydev-nvim.lua
+++ lua/plugins/lsp/lazydev-nvim.lua
@@ -1,6 +1,7 @@
 return {
   { 'Bilal2453/luvit-meta', lazy = true },
   { '45degree/xmake-luals-addon', lazy = true },
+  { 'justinsgithub/wezterm-types', lazy = true },
   {
     'folke/lazydev.nvim',
     ft = 'lua',
@@ -11,6 +12,7 @@ return {
         { path = 'xmake-luals-addon/library', files = { 'xmake.lua' } },
         { path = 'overseer.nvim', mods = { 'overseer' } },
         { path = 'fittencode.nvim', mods = { 'fittencode' } },
+        { path = 'wezterm-types', mods = { 'wezterm' } },
       },
     },
   },
