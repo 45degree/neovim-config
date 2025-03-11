cb0f1434937ed4e0f41b8580cb35cedcf576a7c4
45degree <1098902652@qq.com>
2025-03-11 10:39:53 +0800

    use default branch of markview.nvim

--- lua/plugins/lang/markview-nvim.lua
+++ lua/plugins/lang/markview-nvim.lua
@@ -1,7 +1,6 @@
 return {
   'OXY2DEV/markview.nvim',
   dependencies = 'nvim-treesitter/nvim-treesitter',
-  branch = 'dev',
   submodules = false,
   ft = { 'markdown', 'Avante', 'typst' },
   cmd = 'Markview',
