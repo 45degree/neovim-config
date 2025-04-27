09ca4d0163ac228c4ac091337f77835a17cd684c
45degree <1098902652@qq.com>
2025-04-27 23:38:44 +0800

    feat: add wakatime-vim

---
+++ lua/plugins/code/wakatime-vim.lua
@@ -0,0 +1,5 @@
+return {
+  'wakatime/vim-wakatime',
+  event = 'LazyFile',
+  cmd = { 'WakaTimeApiKey', 'WakaTimeDebugEnable', 'WakaTimeDebugDisable', 'WakaTimeScreenRedrawDisable', 'WakaTimeToday' },
+}
