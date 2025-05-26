22ed2050a81f87510a3727b28444956eae97e4ab
45degree <1098902652@qq.com>
2025-05-26 19:03:52 +0800

    fix: update treesitter branch to main

--- lua/plugins/code/nvim-treesitter.lua
+++ lua/plugins/code/nvim-treesitter.lua
@@ -3,29 +3,7 @@ return {
   event = 'LazyFile',
   cmd = { 'TSUpdate', 'TSInstall', 'TSBufEnable', 'TSBufDisable', 'TSModuleInfo' },
   branch = 'main',
-  opts = {
-    ensure_installed = 'all',
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    auto_install = false,
-    modules = {},
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = function(lang, buf)
-        -- if the file is too large, disable highlight
-        local max_filesize = 100 * 1024 -- 100 KB
-        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-        if ok and stats and stats.size > max_filesize then return true end
-
-        -- if the buf has an attached lsp expected null-ls, disable highlight
-        -- local clients = vim.lsp.get_clients({ bufnr = buf })
-        -- for _, client in ipairs(clients) do
-        --   if client.server_capabilities['semanticTokensProvider'] then return true end
-        -- end
-        return false
-      end,
-      additional_vim_regex_highlighting = false,
-    },
-  },
+  opts = { install_dir = vim.fn.stdpath('data') .. '/site' },
   config = function(_, opts)
     require('nvim-treesitter.parsers').haxe = {
       install_info = {
@@ -36,5 +14,29 @@ return {
       filetype = 'haxe',
     }
     require('nvim-treesitter').setup(opts)
+
+    vim.api.nvim_create_autocmd('BufEnter', {
+      callback = function()
+        local buf = vim.api.nvim_get_current_buf()
+        local parser = vim.treesitter.get_parser(buf, nil, { error = false })
+        if parser == nil then return end
+
+        local max_filesize = 100 * 1024 -- 100 KB
+        local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
+        if ok and stats and stats.size > max_filesize then
+          vim.treesitter.stop(buf)
+          return
+        end
+
+        local clients = vim.lsp.get_clients({ bufnr = buf })
+        for _, client in ipairs(clients) do
+          if client.server_capabilities['semanticTokensProvider'] then
+            vim.treesitter.stop(buf)
+            return
+          end
+        end
+        vim.treesitter.start(buf)
+      end,
+    })
   end,
 }
