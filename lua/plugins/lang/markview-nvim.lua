8bb19f124dbe6acd5ad56c4150ed7c22446c812a
45degree <1098902652@qq.com>
2025-09-26 20:44:06 +0800

    fix: fix warning when open markdown

--- lua/plugins/lang/markview-nvim.lua
+++ lua/plugins/lang/markview-nvim.lua
@@ -1,6 +1,5 @@
 return {
   'OXY2DEV/markview.nvim',
-  dependencies = 'nvim-treesitter/nvim-treesitter',
   submodules = false,
   ft = { 'markdown', 'typst', 'codecompanion' },
   cmd = 'Markview',
