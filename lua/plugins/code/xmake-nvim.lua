128f39f81ff955a81c401772d580b7c3f1ee2c97
45degree <1098902652@qq.com>
2025-10-01 15:33:42 +0800

    fix: remove errorformat for xmake.nvim

--- lua/plugins/code/xmake-nvim.lua
+++ lua/plugins/code/xmake-nvim.lua
@@ -2,15 +2,5 @@ return {
   dir = vim.fn.stdpath('config') .. '/xmake.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   cmd = 'XMake',
-  event = 'VeryLazy',
-  config = function()
-    -- MSBuild:
-    vim.opt.errorformat:append([[\ %#%f(%l\,%c):\ %m]])
-    -- cl.exe:
-    vim.opt.errorformat:append([[\ %#%f(%l)\ :\ %#%t%[A-z]%#\ %m]])
-    -- clang / gcc
-    vim.opt.errorformat:append([[%E%f:%l:%c:\ %trror:\ %m,%-Z%p^,%+C%.%#]])
-
-    require('xmake').setup({})
-  end,
+  config = true,
 }
