21c586ed9bff2ffcbba3f3b1117d02699089dea5
45degree <1098902652@qq.com>
2025-10-08 20:07:32 +0800

    feat: disable markview for codecompanion

--- lua/plugins/lang/markview-nvim.lua
+++ lua/plugins/lang/markview-nvim.lua
@@ -1,7 +1,7 @@
 return {
   'OXY2DEV/markview.nvim',
   submodules = false,
-  ft = { 'markdown', 'typst', 'codecompanion' },
+  ft = { 'markdown', 'typst' },
   cmd = 'Markview',
-  opts = { preview = { enable = true, filetypes = { 'markdown', 'typst', 'codecompanion' }, ignore_buftypes = {} } },
+  opts = { preview = { enable = true, filetypes = { 'markdown', 'typst' }, ignore_buftypes = {} } },
 }
