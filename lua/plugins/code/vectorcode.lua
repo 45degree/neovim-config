c81d3a267ad8885256ddf30adff18e273154bc1b
45degree <1098902652@qq.com>
2025-10-13 14:40:24 +0800

    fix: disable vectorcode if not find

--- lua/plugins/code/vectorcode.lua
+++ lua/plugins/code/vectorcode.lua
@@ -2,6 +2,6 @@ return {
   'Davidyz/VectorCode',
   version = '*',
   build = 'uv tool upgrade vectorcode',
-  cond = function() return vim.fn.executable('vectorcode') == 1 end,
+  enabled = function() return vim.fn.executable('vectorcode') == 1 end,
   lazy = true,
 }
