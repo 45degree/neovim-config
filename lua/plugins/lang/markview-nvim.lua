563450609bc4f4c97f27466e72d5ce1de0e42cce
45degree <1098902652@qq.com>
2025-11-03 10:41:22 +0800

    feat: enable render codecompanion by markview

--- lua/plugins/lang/markview-nvim.lua
+++ lua/plugins/lang/markview-nvim.lua
@@ -1,7 +1,7 @@
 return {
   'OXY2DEV/markview.nvim',
   submodules = false,
-  ft = { 'markdown', 'typst' },
+  ft = { 'markdown', 'typst', 'codecompanion' },
   cmd = 'Markview',
-  opts = { preview = { enable = true, filetypes = { 'markdown', 'typst' }, ignore_buftypes = {} } },
+  opts = { preview = { enable = true, filetypes = { 'markdown', 'typst', 'codecompanion' }, ignore_buftypes = {} } },
 }
