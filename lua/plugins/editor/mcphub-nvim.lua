6659490f01cec7cad00e48a8c230dcb1b227bf03
45degree <1098902652@qq.com>
2025-11-09 21:34:09 +0800

    fix: update mcp-hub config

--- lua/plugins/editor/mcphub-nvim.lua
+++ lua/plugins/editor/mcphub-nvim.lua
@@ -7,6 +7,7 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   build = 'bun install -g mcp-hub@latest', -- Installs `mcp-hub` node binary globally
   opts = {
+    cmd = vim.fn.exepath('mcp-hub'),
     config = vim.fn.stdpath('config') .. '/mcphub/servers.json',
     auto_approve = true,
     global_env = config.env
