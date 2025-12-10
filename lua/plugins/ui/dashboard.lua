22e3eb52d64289f234c9bb6dd255f449ac1722ce
45degree <1098902652@qq.com>
2025-12-10 22:17:20 +0800

    feat: add mason to dashboard

--- lua/plugins/ui/dashboard.lua
+++ lua/plugins/ui/dashboard.lua
@@ -11,6 +11,7 @@ return {
           { icon = ' ', key = 'c', desc = 'Config', action = function() require('snacks.picker').files({ cwd = vim.fn.stdpath('config') }) end },
           { icon = ' ', key = 's', desc = 'Restore Session', action = function() require('session_manager').load_session(false) end },
           { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
+          { icon = ' ', key = 'p', desc = 'Mason', action = ':Mason' },
           { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
         },
       },
