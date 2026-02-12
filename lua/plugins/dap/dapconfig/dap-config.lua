740bbefb4be61c47d7d65e94e21db1f2b39b7b24
45degree <1098902652@qq.com>
2026-02-12 19:40:28 +0800

    fix: dap remove deprecated api

--- lua/plugins/dap/dapconfig/dap-config.lua
+++ lua/plugins/dap/dapconfig/dap-config.lua
@@ -4,7 +4,6 @@ local M = {}
 
 local function config_debuggers()
   -- load from json file
-  require('dap.ext.vscode').load_launchjs(nil, { cppdbg = { 'cpp' } })
   require('plugins.dap.dapconfig.di-cpp')
   require('plugins.dap.dapconfig.di-python')
 end
