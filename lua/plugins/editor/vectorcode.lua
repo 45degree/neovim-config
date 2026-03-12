b5d4b3a71aa56b9cb6f45f097a581cb0fdb0829a
45degree <1098902652@qq.com>
2026-03-12 14:54:31 +0800

    fix: disable vectorcode in opencode

--- lua/plugins/editor/vectorcode.lua
+++ lua/plugins/editor/vectorcode.lua
@@ -1,7 +1,9 @@
+local config = require('config')
+
 return {
   'Davidyz/VectorCode',
   version = '*',
   build = 'uv tool upgrade vectorcode',
-  enabled = function() return vim.fn.executable('vectorcode') == 1 end,
+  enabled = function() return vim.fn.executable('vectorcode') == 1 and config.ai_code_agent.name == 'codecompanion' end,
   lazy = true,
 }
