2ec052fb20b6f38cc6b61bc4a9a0f20eb8236675
45degree <1098902652@qq.com>
2026-05-11 17:16:02 +0800

    feat: use neotest-ctest instead neotest-gtest

--- lua/plugins/lang/neotest-gtest-nvim.lua
+++ lua/plugins/lang/neotest-gtest-nvim.lua
@@ -1,33 +1,8 @@
-local c_cxx_file_extensions = {
-  ['cpp'] = true,
-  ['cppm'] = true,
-  ['cc'] = true,
-  ['cxx'] = true,
-  ['c++'] = true,
-}
-
 return {
   {
-    'alfaix/neotest-gtest',
+    'orjangj/neotest-ctest',
     lazy = true,
-    opts = {
-      ---@param file_path string
-      is_test_file = function(file_path)
-        local sep = '/'
-        if vim.fn.has('win32') == 1 then sep = '\\' end
-
-        local elems = vim.split(file_path, sep, { plain = true })
-        local filename = elems[#elems]:lower()
-        if filename == '' then -- directory
-          return false
-        end
-        local extsplit = vim.split(filename, '.', { plain = true })
-        local extension = extsplit[#extsplit]
-        if not c_cxx_file_extensions[extension] then return false end
-        local stem = extsplit[#extsplit - 1]
-        return vim.startswith(filename, 'test') or vim.endswith(stem, 'test')
-      end,
-    },
+    opts = {},
   },
 
   {
@@ -36,7 +11,7 @@ return {
     opts = function(_, opts)
       opts = opts or {}
       opts.adapters = opts.adapters or {}
-      opts.adapters = vim.tbl_deep_extend('force', opts.adapters, { require('neotest-gtest') })
+      opts.adapters = vim.tbl_deep_extend('force', opts.adapters, { require('neotest-ctest') })
     end,
   },
 }
