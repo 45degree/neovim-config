e749757cc6422cf034e7adcb0dc848ba1107aa2b
45degree <1098902652@qq.com>
2026-05-17 02:47:50 +0800

    feat: update neotest keymap

--- lua/plugins/editor/neotest-nvim.lua
+++ lua/plugins/editor/neotest-nvim.lua
@@ -2,5 +2,25 @@ return {
   'nvim-neotest/neotest',
   cmd = 'Neotest',
   dependencies = { 'nvim-neotest/nvim-nio', 'nvim-lua/plenary.nvim' },
-  config = true,
+  opts = {
+    summary = {
+      mappings = {
+        jumpto = 'o',
+        output = 'p',
+        short = 'P',
+      },
+    },
+  },
+  config = function(_, opts)
+    require('neotest').setup(opts)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('neotest_close_with_q', { clear = true }),
+      pattern = { 'neotest-summary', 'neotest-output', 'neotest-output-panel' },
+      callback = function(event)
+        vim.bo[event.buf].buflisted = false
+        vim.keymap.set('n', 'q', '<cmd>close<cr>', { buffer = event.buf, silent = true })
+      end,
+    })
+  end,
 }
