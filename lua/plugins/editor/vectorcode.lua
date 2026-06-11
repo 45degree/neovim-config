42c28afc88a594f335021c3165ed7229be1f6863
45degree <1098902652@qq.com>
2026-06-11 14:42:58 +0800

    feat: remove vectorcode

--- lua/plugins/editor/vectorcode.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'Davidyz/VectorCode',
-  version = '*',
-  build = 'uv tool upgrade vectorcode',
-  enabled = function() return vim.fn.executable('vectorcode') == 1 end,
-  lazy = true,
-}
